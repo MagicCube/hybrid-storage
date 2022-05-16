8f21ffe66490bcca9567151b46bfe21dc7aa9b57
Henry Li <henry1943@163.com>
2022-05-16 10:47:08 +0800

    feat: add Synchronize

--- src/typings/util.d.ts
+++ src/typings/util.d.ts
@@ -3,3 +3,7 @@ type Nullable<T> = T | null;
 type Maybe<T> = T | undefined;
 
 type MaybeNullable<T> = T | null | undefined;
+
+type Synchronize<T> = T extends (...args: infer P) => Promise<infer R>
+  ? (...args: P) => R
+  : T;
