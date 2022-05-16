4e658cdc32508b614ef5772338759814361c1b3a
Henry Li <henry1943@163.com>
2022-05-16 21:03:47 +0800

    docs: add comments

--- src/storing/ali-oss-storage/AliOSSStorage.ts
+++ src/storing/ali-oss-storage/AliOSSStorage.ts
@@ -32,17 +32,22 @@ export class AliOSSStorage implements AsyncStorage {
   ): Promise<Maybe<Serializable>> {
     let jsonString: string;
     try {
+      // 根据 `key` 获取文件名（含路径）
       const fileName = this._getFileName(key);
+      // 从 OSS 服务中获取文件内容
       const { content } = await this.oss.get(fileName);
       jsonString = content.toString();
     } catch (e) {
       if (e instanceof Error && e.name === 'NoSuchKeyError') {
-        // File not found
+        // OSS 服务报告”NoSuchKeyError“错误，
+        // 即文件路径不存在，则返回默认值
         return defaultValue;
       } else {
+        // 其他的异常照样抛出
         throw e;
       }
     }
+    // 将 JSON 字符串反序列化为值
     const result = this._serializer.deserialize(jsonString);
     return result;
   }
@@ -54,13 +59,19 @@ export class AliOSSStorage implements AsyncStorage {
     if (value === undefined) {
       throw new Error('Undefined value is not allowed.');
     }
+    // 序列化为 JSON 字符串
     const jsonString = this._serializer.serialize(value);
+    // 封装为 Blob 对象
     const blob = new Blob([jsonString], { type: 'application/json' });
+    // 根据 `key` 生成对应的文件名（含路径）
     const fileName = this._getFileName(key);
+    // 执行上传，OSS 会自行决定是新建还是覆盖文件
     const result = await this.oss.put(fileName, blob);
+    // 如果上传成功，则从 HTTP 响应头中获得 etag，并将其返回。
     const { etag: etagWithDoubleQuotes } = result.res.headers as {
       etag: string;
     };
+    // 要注意的是 OSS SDK 返回的 etag 两头会各多一个英文引号，我们要去掉它们。
     return dropQuotes(etagWithDoubleQuotes);
   }
 
@@ -68,6 +79,9 @@ export class AliOSSStorage implements AsyncStorage {
    * @inheritdoc
    */
   async getMetaIndex(): Promise<StorageMetaIndex> {
+    // `list` 方法可以根据前缀，查询文件的元数据信息，
+    // 其中就包含了 etag 信息。
+    // 单次查询最多可一次性返回 1000 个文件的信息。
     const list = await this.oss.list(
       {
         prefix: `${this.instanceName}/`,
