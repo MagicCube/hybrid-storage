b63930f9e05ac82a6f4cfd4b52640d2f83fb3eda
Henry Li <henry1943@163.com>
2022-05-16 10:49:22 +0800

    docs: add comments

--- src/typings/util.d.ts
+++ src/typings/util.d.ts
@@ -1,9 +1,21 @@
+/**
+ * 泛型工具类：返回可为 `null` 的 `T` 类型。
+ */
 type Nullable<T> = T | null;
 
+/**
+ * 泛型工具类：返回可为 `undefined` 的 `T` 类型。
+ */
 type Maybe<T> = T | undefined;
 
+/**
+ * 泛型工具类：返回可为 `null` 或 `undefined` 的 `T` 类型。
+ */
 type MaybeNullable<T> = T | null | undefined;
 
-type Synchronize<T> = T extends (...args: infer P) => Promise<infer R>
+/**
+ * 泛型工具类：将一个返回值为 `Promise<T>` 的函数，转换为返回值为 `T` 类型的函数。
+ */
+type Unpromisify<T> = T extends (...args: infer P) => Promise<infer R>
   ? (...args: P) => R
   : T;
